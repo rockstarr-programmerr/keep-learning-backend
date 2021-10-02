9a5365fe89132bab8f96f9cf0fdb063b0db34eb4
rockstar-programmer <rockstarrprogrammerr@gmail.com>
2021-10-02 22:36:02 +0700

    account models

---
+++ account/managers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import UserManager
+
+
+def get_first_part_of_email(email):
+    return email.split('@')[0]
+
+
+class CustomUserManager(UserManager):
+    """
+    Customize to allow creating user with just email, no need username
+    """
+    def create_user(self, username=None, email=None, password=None, **extra_fields):
+        assert bool(email), 'Email is required for creating user.'
+        username = get_first_part_of_email(email)
+        return super().create_user(username, email=email, password=password, **extra_fields)
+
+    def create_superuser(self, username=None, email=None, password=None, **extra_fields):
+        assert bool(email), 'Email is required for creating user.'
+        username = get_first_part_of_email(email)
+        return super().create_superuser(username, email=email, password=password, **extra_fields)
--- account/models.py
+++ account/models.py
@@ -1,3 +1,65 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import validate_image_file_extension
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from PIL import Image
 
-# Create your models here.
+from account.managers import CustomUserManager
+
+AVATAR_WIDTH = 256
+AVATAR_HEIGHT = 256
+AVATAR_THUMBNAIL_WIDTH = 64
+AVATAR_THUMBNAIL_HEIGHT = 64
+
+
+class User(AbstractUser):
+    email = models.EmailField(
+        unique=True,
+        error_messages={
+            'unique': _('A user with that email already exists.'),
+        },
+    )
+    name = models.CharField(
+        max_length=150,
+    )
+    phone_number = models.CharField(
+        max_length=20,
+        blank=True,
+    )
+    avatar = models.ImageField(
+        upload_to='users/avatar/%Y/%m',
+        blank=True,
+        validators=[validate_image_file_extension],
+    )
+    avatar_thumbnail = models.ImageField(
+        upload_to='users/avatar_thumbnail/%Y/%m',
+        blank=True,
+        validators=[validate_image_file_extension],
+    )
+    username = models.CharField(
+        max_length=150,
+        blank=True,
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f'{self.name} ({self.email})'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self._make_thumbnail(self.avatar, AVATAR_WIDTH, AVATAR_HEIGHT)
+        self._make_thumbnail(self.avatar_thumbnail, AVATAR_THUMBNAIL_WIDTH, AVATAR_THUMBNAIL_HEIGHT)
+
+    @staticmethod
+    def _make_thumbnail(image, width, height):
+        if image and (
+            image.width > width or
+            image.height > height
+        ):
+            with Image.open(image.path) as f:
+                f.thumbnail((width, height))
+                f.save(image.path)
